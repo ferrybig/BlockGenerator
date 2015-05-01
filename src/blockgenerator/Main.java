f271a973fdc023d55a012d5f863eb239222b6d66
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2015-05-01 23:22:01 +0200

    Changed Colors to simpler example colors

--- src/blockgenerator/Main.java
+++ src/blockgenerator/Main.java
@@ -15,26 +15,26 @@ import javax.imageio.ImageIO;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        MultiColor background = new MultiColor(Color.green.darker(), Color.green.darker().darker().darker());
+        MultiColor background = new MultiColor(Color.yellow.darker(), Color.yellow.darker().darker());
         MultiColor border = new MultiColor(Color.orange, Color.orange.darker().darker());
         MultiColor sides = new MultiColor(Color.blue, Color.blue.darker().darker());
-        MultiColor middle = new MultiColor(Color.red, Color.red.darker().darker());
+        MultiColor middle = new MultiColor(Color.yellow.brighter(), Color.yellow);
         MultiColor heart = new MultiColor(new Color(164,16,16), new Color(164,16,16).darker().darker());
-        int height = 256;
+        int height = 1024;
         int squareSize = 8;
         int paddingSize = 2;
         boolean skipBorders = true;
         Random random = new Random();
         MainGrid grid = new MainGrid(
                 background,
-                new BlockPattern(random, 128, height, border, 128 / squareSize, height / squareSize, paddingSize, skipBorders, false, true),
-                new BlockPattern(random, 128, height, sides, 128 / squareSize, height / squareSize, paddingSize, skipBorders, false, true),
                 new BlockPattern(random, 128, height, middle, 128 / squareSize, height / squareSize, paddingSize, skipBorders, false, true),
-                new BlockPattern(random, 128, height, heart, 128 / squareSize, height / squareSize, paddingSize, skipBorders, false, true),
-                //new SolidColorPattern(0, height, middle),
-                new BlockPattern(random, 128, height, middle, 128 / squareSize, height / squareSize, paddingSize, skipBorders, false, true),
-                new BlockPattern(random, 128, height, sides, 128 / squareSize, height / squareSize, paddingSize, skipBorders, false, true),
-                new BlockPattern(random, 128, height, border, 128 / squareSize, height / squareSize, paddingSize, skipBorders, false, true),
+                //new BlockPattern(random, 128, height, sides, 128 / squareSize, height / squareSize, paddingSize, skipBorders, false, true),
+                //new BlockPattern(random, 128, height, middle, 128 / squareSize, height / squareSize, paddingSize, skipBorders, false, true),
+                //new BlockPattern(random, 128, height, heart, 128 / squareSize, height / squareSize, paddingSize, skipBorders, false, true),
+                new SolidColorPattern(768, height, middle),
+                //new BlockPattern(random, 128, height, middle, 128 / squareSize, height / squareSize, paddingSize, skipBorders, false, true),
+                //new BlockPattern(random, 128, height, sides, 128 / squareSize, height / squareSize, paddingSize, skipBorders, false, true),
+                //new BlockPattern(random, 128, height, border, 128 / squareSize, height / squareSize, paddingSize, skipBorders, false, true),
                 new BlockPattern(random, 128, height, background, 128 / squareSize, height / squareSize, paddingSize, skipBorders, false, true)
         );
 
